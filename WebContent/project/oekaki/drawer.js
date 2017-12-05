bb96c3470bc9d5a316f08f434c3f36723eef8fd0
tan1510 <1510370222u@kindai.ac.jp>
2017-12-06 03:59:06 +0900

    firstcommit
    線を引ける機能だけの部分の実装

---
+++ WebContent/project/oekaki/drawer.js
@@ -0,0 +1,47 @@
+//変数
+
+//キャンバスの部分
+var canvas = document.getElementById('canvas');
+var c_context = canvas.getContext('2d');
+
+var drawing = false;  //
+var b_x = 0;　//描画した際の最後のx座標
+var b_y = 0;　//描画した際の最後のy座標
+
+//マウスが動いている時描画
+canvas.addEventListener('mousemove',drawCanvas);
+
+canvas.addEventListener('mousedown',function(e) {
+    drawing = true;
+    var rect = e.target.getBoundingClientRect();
+    b_x = e.clientX - rect.left;
+    b_y = e.clientY - rect.top;
+});
+//マウスを話した時drawingをfalseにする
+canvas.addEventListener('mouseup',function(){
+    drawing = false;
+})
+
+
+//mousemoveの時に実行する関数
+function drawCanvas(e){
+    if(!drawing){
+        return
+    };
+//マウスが動いた位置に連続で描画していく
+    var rect = e.target.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+
+    c_context.lineCap = 'round';
+    c_context.strokeStyle = 'black';
+    c_context.lineWidth = '10px';
+    c_context.beginPath();
+    c_context.moveTo(b_x, b_y);
+    c_context.lineTo(x, y);
+    c_context.stroke();
+    c_context.closePath();
+
+    b_x = x;
+    b_y = y;
+}

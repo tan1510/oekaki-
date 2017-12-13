2adbeab425be96b6e9e84d266594ff7bf83eb19b
tan1510 <1510370222u@kindai.ac.jp>
2017-12-13 15:34:19 +0900

    UploaderServletの修正

--- src/project/oekaki/UploadServlet.java
+++ src/project/oekaki/UploadServlet.java
@@ -1,23 +1,26 @@
 package project.oekaki;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import java.util.Base64;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @WebServlet("/project/oekaki/echo")
-public class UploadServlet extends HttpServelet{
+public class UploadServlet extends HttpServlet{
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println(request.getParamater("messeage"));
-        byte[] bytes = request.getParamater("messeage");
-        System.out.println(bytes);
+        System.out.println(request.getParameter("messeage"));
+        String str= request.getParameter("messege");
+        System.out.println(str);
 		
 	}
 }

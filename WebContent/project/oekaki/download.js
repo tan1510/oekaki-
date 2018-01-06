ff1331c1858d79cd05b93a87b203ad0cc9ebf492
tan1510 <1510370222u@kindai.ac.jp>
2018-01-06 15:55:18 +0900

    仮完成
    仮完成
    1/6

--- WebContent/project/oekaki/download.js
+++ WebContent/project/oekaki/download.js
@@ -16,17 +16,23 @@ function receive() {
 		var response = JSON.parse(xmlHttpRequest.responseText);
 		var namelist = response.namelist;
 		var names = namelist.split(',');
+		names.shift();
 		console.log(names);
 		document.open();
-		for(var i = names.length;i--;){
+		document.write('<link rel="stylesheet" type="text/css" href="download.css"/>');	
+		document.write('<input type ="button"　 id="start_draw" value="戻る" onclick="location.href=\'http://localhost:8080/isp2/project/oekaki/title.html\'"><br>')
+			for(var i = names.length;i--;){
 			var name = names[i].split('.');
-			document.write('<input type ="button"　 id="start_draw" value="'+name[0]+'" onclick="location.href=\'http://localhost:8080/isp2/project/oekaki/save/'+name[0]+'.png\'"\'><br>');
-		}
+			
+		//	document.write('<input type ="button"　 id="start_draw" value="'+name[0]+'" onclick="location.href=\'http://localhost:8080/isp2/project/oekaki/save/'+name[0]+'.png\'"\'><br>');
+			document.write('<a href ="http://localhost:8080/isp2/project/oekaki/save/'+name[0]+'.png" download="'+name[0]+'.png">'+name[0]+'</a><br>');
+	}
 		document.close();
 	}
 }
 window.addEventListener("load",function() { 
     var postButtonElement = document.getElementById("get_data");
     postButtonElement.addEventListener("click", sendWithPostMethod, false);
-
 },false);
+
+sendWithPostMethod();
--- WebContent/project/oekaki/drawer.js
+++ WebContent/project/oekaki/drawer.js
@@ -1,25 +1,43 @@
+//tachib
 //変数
+//消しゴムが有効になっているか
 var eraserActivated = false;
 //キャンバスの部分
 var canvas = document.getElementById('canvas');
 var c_context = canvas.getContext('2d');
 c_context.fillStyle = 'rgb(255,255,255)';
     c_context.fillRect(0,0,512,512);
-	   
+
 var drawing = false;  //
 var b_x = 0;　//描画した際の最後のx座標
 var b_y = 0;　//描画した際の最後のy座標
 
+
+onload = function() {
+    draw();
+  };
+  function draw() {
+    var canvas = document.getElementById('c1');
+    if ( ! canvas || ! canvas.getContext ) { return false; }
+    var ctx = canvas.getContext('2d');
+    /* Imageオブジェクトを生成 */
+    var img = new Image();
+    img.src = "image1.gif";
+    /* 画像を描画 */
+    ctx.drawImage(img, 0, 0);
+  }
+//合成方式の変更によって消すことを実現してたけどフラグ管理して消しゴム
+//が有効になっている時白で描くように変更
 function eraser(){
     if(eraserActivated){
-        c_context.globalCompositeOperation = 'source-over';
+   //     c_context.globalCompositeOperation = 'source-over';
         eraserActivated=false;
     }else{
-        c_context.globalCompositeOperation  = 'destination-out';
+   //     c_context.globalCompositeOperation  = 'destination-out';
         eraserActivated=true;
     }
     }
-
+//マウスをクリックした時の処理
 canvas.addEventListener('mousedown',function(e) {
     drawing = true;
     var rect = e.target.getBoundingClientRect();
@@ -34,11 +52,18 @@ canvas.addEventListener('mouseup',function(){
 
 //mousemoveの時に実行する関数
 function drawCanvas(e){
-    var color = document.getElementById('color').value; //追加
+    var color
+    if(!eraserActivated){
+        color = document.getElementById('color').value; //色を取得
+    }else{
+        color = 'white'
+    }
     c_context.strokeStyle = color; //変更
     if(!drawing){
         return
     };
+
+    var w = document.getElementById('width').value; //太さを取得
 //マウスが動いた位置に連続で描画していく
     var rect = e.target.getBoundingClientRect();
     var x = e.clientX - rect.left;
@@ -46,7 +71,7 @@ function drawCanvas(e){
 
     c_context.lineCap = 'round';
  //   c_context.strokeStyle = 'red';
-    c_context.lineWidth = '10px';
+    c_context.lineWidth = w;
     c_context.beginPath();
     c_context.moveTo(b_x, b_y);
     c_context.lineTo(x, y);
